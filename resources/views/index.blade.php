48ab998afc531f4b1660dbe15f0085e39ac036ef
David Guras <gura.san@gmail.com>
2020-08-01 23:31:08 -0400

    finish content section mobile layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
         <h2 class="text-blue-500 uppercase tracking-wide font-semibold">
             Popular Games
         </h2>
-        <div class="popular-games text-sm grid grid-cols-6 gap-12 border-b border-gray-800 pb-16">
+        <div class="popular-games text-sm grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-6 gap-12 border-b border-gray-800 pb-16">
             <div class="game mt-8">
                 <div class="relative inline-block">
                     <a href="#">
@@ -98,8 +98,8 @@
                 <div class="text-gray-400 mt-1">Playstation 4</div>
             </div>
         </div> <!-- end popular games -->
-        <div class="flex my-10">
-            <div class="recently-reviewed w-3/4 mr-32">
+        <div class="flex flex-col lg:flex-row my-10">
+            <div class="recently-reviewed w-full lg:w-3/4 mr-0 lg:mr-32">
                 <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Recently Reviewed</h2>
                 <div class="recently-reviewed-container space-y-12 mt-8">
                     <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
@@ -117,48 +117,12 @@
                         <div class="ml-12">
                             <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">Final Fantasy 7 Remake</a>
                             <div class="text-gray-400 mt-1">Playstation 4</div>
-                            <div class="text-gray-400 mt-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, debitis dolor doloremque ea harum, itaque laudantium magnam magni maiores minus nesciunt nostrum quae quaerat quo sunt veritatis voluptate. Ex, sapiente.</div>
-                        </div>
-                    </div>
-                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
-                        <div class="relative flex-none">
-                            <a href="#">
-                                <img src="/ff7.jpg" alt="game cover"
-                                     class="w-48 hover:opacity-75 transition ease-in-out duration-150">
-                            </a>
-                            <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full"
-                                 style="right:-20px;bottom:-20px;">
-                                <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
-
-                            </div>
-                        </div>
-                        <div class="ml-12">
-                            <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">Final Fantasy 7 Remake</a>
-                            <div class="text-gray-400 mt-1">Playstation 4</div>
-                            <div class="text-gray-400 mt-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, debitis dolor doloremque ea harum, itaque laudantium magnam magni maiores minus nesciunt nostrum quae quaerat quo sunt veritatis voluptate. Ex, sapiente.</div>
-                        </div>
-                    </div>
-                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
-                        <div class="relative flex-none">
-                            <a href="#">
-                                <img src="/ff7.jpg" alt="game cover"
-                                     class="w-48 hover:opacity-75 transition ease-in-out duration-150">
-                            </a>
-                            <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full"
-                                 style="right:-20px;bottom:-20px;">
-                                <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
-
-                            </div>
-                        </div>
-                        <div class="ml-12">
-                            <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">Final Fantasy 7 Remake</a>
-                            <div class="text-gray-400 mt-1">Playstation 4</div>
-                            <div class="text-gray-400 mt-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, debitis dolor doloremque ea harum, itaque laudantium magnam magni maiores minus nesciunt nostrum quae quaerat quo sunt veritatis voluptate. Ex, sapiente.</div>
+                            <div class="text-gray-400 mt-6 hidden lg:block">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, debitis dolor doloremque ea harum, itaque laudantium magnam magni maiores minus nesciunt nostrum quae quaerat quo sunt veritatis voluptate. Ex, sapiente.</div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="most-anticipated w-1/4 ">
+            <div class="most-anticipated  lg:w-1/4 mt-12 lg:mt-0">
                 <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Most Anticipated</h2>
                 <div class="most-anticipated-container space-y-10 mt-8">
                     <div class="game flex">

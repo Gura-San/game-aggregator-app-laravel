d877124e65f2aa578358a85546143c1c91859f72
David Guras <gura.san@gmail.com>
2020-08-04 23:56:21 -0400

    finalize front page create imdb queries for all elements output info on front page
    connect IGDB api
    create GamesController with API queries
    Populate API data on index page [WIP]

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,136 +6,114 @@
             Popular Games
         </h2>
         <div class="popular-games text-sm grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-6 gap-12 border-b border-gray-800 pb-16">
+            @foreach($popularGames as $game)
             <div class="game mt-8">
                 <div class="relative inline-block">
                     <a href="#">
-                        <img src="/ff7.jpg" alt="game cover"
+                        <img src="{{ Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']) }}"
+                             alt="game cover for {{ $game['name'] }}"
                              class="hover:opacity-75 transition ease-in-out duration-150">
                     </a>
+                    @if(isset($game['rating']))
                     <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
                          style="right:-20px;bottom:-20px;">
-                        <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
-
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">
+                            {{ round($game['rating']).'%' }}
+                        </div>
                     </div>
+                    @endif
                 </div>
-                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy 7
-                    Remake</a>
-                <div class="text-gray-400 mt-1">Playstation 4</div>
-            </div>
-            <div class="game mt-8">
-                <div class="relative inline-block">
-                    <a href="#">
-                        <img src="/ff7.jpg" alt="game cover"
-                             class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
-                         style="right:-20px;bottom:-20px;">
-                        <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
-
-                    </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+                    {{ $game['name'] }}
+                </a>
+                <div class="text-gray-400 mt-1">
+                    @foreach($game['platforms'] as $platform)
+                        @if(array_key_exists('abbreviation', $platform))
+                        {{ $platform['abbreviation'] }},
+                        @endif
+                    @endforeach
                 </div>
-                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy 7 Remake</a>
-                <div class="text-gray-400 mt-1">Playstation 4</div>
             </div>
-            <div class="game mt-8">
-                <div class="relative inline-block">
-                    <a href="#">
-                        <img src="/ff7.jpg" alt="game cover"
-                             class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
-                         style="right:-20px;bottom:-20px;">
-                        <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
-
-                    </div>
-                </div>
-                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy 7 Remake</a>
-                <div class="text-gray-400 mt-1">Playstation 4</div>
-            </div>
-            <div class="game mt-8">
-                <div class="relative inline-block">
-                    <a href="#">
-                        <img src="/ff7.jpg" alt="game cover"
-                             class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
-                         style="right:-20px;bottom:-20px;">
-                        <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
-
-                    </div>
-                </div>
-                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy 7 Remake</a>
-                <div class="text-gray-400 mt-1">Playstation 4</div>
-            </div>
-            <div class="game mt-8">
-                <div class="relative inline-block">
-                    <a href="#">
-                        <img src="/ff7.jpg" alt="game cover"
-                             class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
-                         style="right:-20px;bottom:-20px;">
-                        <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
-
-                    </div>
-                </div>
-                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy 7 Remake</a>
-                <div class="text-gray-400 mt-1">Playstation 4</div>
-            </div>
-            <div class="game mt-8">
-                <div class="relative inline-block">
-                    <a href="#">
-                        <img src="/ff7.jpg" alt="game cover"
-                             class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
-                         style="right:-20px;bottom:-20px;">
-                        <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
-
-                    </div>
-                </div>
-                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy 7 Remake</a>
-                <div class="text-gray-400 mt-1">Playstation 4</div>
-            </div>
+            @endforeach
         </div> <!-- end popular games -->
+
         <div class="flex flex-col lg:flex-row my-10">
             <div class="recently-reviewed w-full lg:w-3/4 mr-0 lg:mr-32">
                 <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Recently Reviewed</h2>
+                @foreach($recentlyReviewed as $review)
                 <div class="recently-reviewed-container space-y-12 mt-8">
                     <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
                         <div class="relative flex-none">
                             <a href="#">
-                                <img src="/ff7.jpg" alt="game cover"
-                                     class="w-48 hover:opacity-75 transition ease-in-out duration-150">
+                                <img src="{{ Str::replaceFirst('thumb', 'cover_big', $review['cover']['url']) }}"
+                                     class="w-48 hover:opacity-75 transition ease-in-out duration-150"
+                                     alt="game cover for {{ $review['name'] }}">
                             </a>
+                            @if (isset($review['rating']))
                             <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full"
                                  style="right:-20px;bottom:-20px;">
-                                <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
-
+                                <div class="font-semibold text-xs flex justify-center items-center h-full">
+                                    {{ round($review['rating']).'%' }}
+                                </div>
                             </div>
+                            @endif
                         </div>
                         <div class="ml-12">
-                            <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">Final Fantasy 7 Remake</a>
-                            <div class="text-gray-400 mt-1">Playstation 4</div>
-                            <div class="text-gray-400 mt-6 hidden lg:block">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, debitis dolor doloremque ea harum, itaque laudantium magnam magni maiores minus nesciunt nostrum quae quaerat quo sunt veritatis voluptate. Ex, sapiente.</div>
+                            <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">
+                                {{ $review['name'] }}
+                            </a>
+                            <div class="text-gray-400 mt-1">
+                                @foreach($review['platforms'] as $reviewPlatform)
+                                    @if(array_key_exists('abbreviation', $reviewPlatform))
+                                        {{ $reviewPlatform['abbreviation'] }},
+                                    @endif
+                                @endforeach
+                            </div>
+                            <div class="text-gray-400 mt-6 hidden md:block">
+								{{ $review['summary'] }}
+                            </div>
                         </div>
                     </div>
                 </div>
+                @endforeach
             </div>
             <div class="most-anticipated  lg:w-1/4 mt-12 lg:mt-0">
                 <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Most Anticipated</h2>
+                @foreach($mostAnticipated as $anticipated)
                 <div class="most-anticipated-container space-y-10 mt-8">
                     <div class="game flex">
                         <a href="#">
-                            <img src="/ff7.jpg" alt="game cover"
-                                 class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                            <img src="{{ Str::replaceFirst('thumb', 'cover_small', $anticipated['cover']['url']) }}"
+                                 class="w-16 hover:opacity-75 transition ease-in-out duration-150"
+                                 alt="game cover for {{ $anticipated['name'] }}">
                         </a>
                         <div class="ml-4">
-                            <a href="#" class="hover:text-gray-300">Final Fantasy 7 Remake</a>
-                            <div class="text-gray-400 text-sm mt-1">Sept 20, 2020</div>
+                            <a href="#" class="hover:text-gray-300">{{ $anticipated['name'] }}</a>
+                            <div class="text-gray-400 text-sm mt-1">
+                                {{ Carbon\Carbon::parse($anticipated['first_release_date'])->format('M d, Y') }}
+                            </div>
                         </div>
                     </div>
                 </div>
+                @endforeach
+                <h2 class="text-blue-500 uppercase tracking-wide font-semibold mt-12">Coming Soon</h2>
+                @foreach($comingSoon as $soon)
+                    <div class="most-anticipated-container space-y-10 mt-8">
+                        <div class="game flex">
+                            <a href="#">
+                                <img src="{{ Str::replaceFirst('thumb', 'cover_small', $soon['cover']['url']) }}"
+                                     class="w-16 hover:opacity-75 transition ease-in-out duration-150"
+                                     alt="game cover for {{ $soon['name'] }}">
+                            </a>
+                            <div class="ml-4">
+                                <a href="#" class="hover:text-gray-300">{{ $soon['name'] }}</a>
+                                <div class="text-gray-400 text-sm mt-1">
+                                    {{ Carbon\Carbon::parse($soon['first_release_date'])->format('M d, Y') }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div> <!-- end container -->
